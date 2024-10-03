7aef72a15e467b55c3d40fd45048aa81e54bf248
mercy-mwai <mercyneemam@gmail.com>
2024-10-03 19:52:31 +0300

    modified my JavaScript and html code

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,5 +1,6 @@
 function updateTime(){
 let nairobiElement=document.querySelector("#nairobi");
+if(nairobiElement){
 let nairobiDateElement=nairobiElement.querySelector(".date");
 let nairobiTimeElement=nairobiElement.querySelector(".time");
 let nairobiTime= moment().tz("Africa/Nairobi");
@@ -7,9 +8,11 @@ let nairobiTime= moment().tz("Africa/Nairobi");
 nairobiDateElement.innerHTML=nairobiTime.format("MMMM Do ,YYYY");
 
 nairobiTimeElement.innerHTML=nairobiTime.format("h:mm:ss [<small>]A[<small/>]");
+}
 
-
+  
 let sydneyElement=document.querySelector("#sydney");
+if(sydneyElement){
 let sydneyDateElement=sydneyElement.querySelector(".date");
 let sydneyTimeElement=sydneyElement.querySelector(".time");
 let sydneyTime= moment().tz("Australia/Sydney");
@@ -17,24 +20,26 @@ let sydneyTime= moment().tz("Australia/Sydney");
 sydneyDateElement.innerHTML=sydneyTime.format("MMMM Do ,YYYY");
 
 sydneyTimeElement.innerHTML=sydneyTime.format("h:mm:ss [<small>]A[<small/>]");
+}
 };
-function updateCity(event){
-    let cityTimeZone=event.target.value;
-    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
-    let cityTime = moment().tz(cityTimeZone);
-    let citiesElement = document.querySelector("#cities");
-    citiesElement.innerHTML= `
-    <div class="city">
-      <div>
-        <h2>${cityName}</h2>
-        <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
-      </div>
-      <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
-      "A"
-    )}</small></div>
-    </div>
-    `;
-};
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  if (cityTimeZone) {
+      let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+      let cityTime = moment().tz(cityTimeZone);
+      let citiesElement = document.querySelector("#cities-container");
+      citiesElement.innerHTML = `
+          <div class="city">
+              <div>
+                  <h2>${cityName}</h2>
+                  <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+              </div>
+              <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
+          </div>
+                `;
+    }
+}
 
 updateTime();
 setInterval(updateTime , 1000);
