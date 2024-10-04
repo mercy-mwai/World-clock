f73fb429746eacad8aca69e72ac4a81284bd0473
mercy-mwai <mercyneemam@gmail.com>
2024-10-04 13:43:56 +0300

    More JavaScript code

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -28,18 +28,19 @@ function updateCity(event) {
   if(cityTimeZone === "current"){
     cityTimeZone=moment.tz.guess();
   }
-      let cityName = cityTimeZone.replace("_", " ").split("/")[1];
-      let cityTime = moment().tz(cityTimeZone);
-      let citiesElement = document.querySelector("#cities-container");
-      citiesElement.innerHTML = `
-          <div class="city">
-              <div>
-                  <h2>${cityName}</h2>
-                  <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
-              </div>
-              <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
-          </div>
-                `;
+let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+let cityTime = moment().tz(cityTimeZone);
+let citiesElement = document.querySelector("#cities-container");
+citiesElement.innerHTML = `
+   <div class="city">
+        <div>
+          <h2>${cityName}</h2>
+          <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+        </div>
+        <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
+   </div>
+          <a href="/">All cities</a>
+            `;
 
 };
 
