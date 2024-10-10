9cda772b7d72548cfba4474499d8bac3843856e4
mercy-mwai <mercyneemam@gmail.com>
2024-10-10 07:07:20 +0300

    updated my Sydney element

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -32,6 +32,7 @@ let sydneyTimeElement=sydneyElement.querySelector(".time");
 let sydneyTime= moment().tz("Australia/Sydney");
 
 sydneyDateElement.innerHTML=sydneyTime.format("MMMM Do ,YYYY");
+sydneyTimeElement.innerHTML=sydneyTime.format("h:mm:ss [<small>]A[<small/>]");
 
 }
 };
